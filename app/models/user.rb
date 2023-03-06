742db451a09ce32bf8076e5c2ec7c9afaa0f55b2
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-06 16:00:11 +0900

    正規表現によるバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,20 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :kana_last_name, presence: true
-  validates :kana_first_name, presence: true
   validates :birthday, presence: true
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
   
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/ } do
+    validates :kana_first_name
+    validates :kana_last_name
+  end
+  
+
 end
