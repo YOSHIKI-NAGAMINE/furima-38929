7b550b7d517bc920891b4cda763ec0b19114de97
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-07 16:50:24 +0900

    単体テスト準備

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it  do
+      end
+    end
+    context '新規登録できないとき' do
+      it  do
+      end
+    end
+end
