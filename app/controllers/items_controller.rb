ca1d939bd9027283aa5e3a13849cdacf6cf10600
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-10 16:36:20 +0900

    出品の可否によるページ遷移設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render action: :new
+    end
   end
 
 private
