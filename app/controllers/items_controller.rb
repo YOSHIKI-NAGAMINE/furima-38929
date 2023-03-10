ff8ee3ddedf345029bf2ee0ff101a532141c30f0
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-10 16:11:00 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
 private
 
   def item_params
-    params.require(:item).permit(:name, :explain, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price)
+    params.require(:item).permit(:name, :explain, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price, :image)
   end
 
 end
