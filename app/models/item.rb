c33f9b0d7639a2c873a9ce2156b04a917d5c388b
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-10 15:06:31 +0900

    アクティブハッシュ導入（カテゴリー）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,17 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 
   validates :name, presence: true
   validates :explain, presence: true
-  validates :category_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, presence: true
   validates :shipping_charge_id, presence: true
   validates :prefecture_id, presence: true
   validates :shipping_day_id, presence: true
   validates :price, presence: true
 
+
+  belongs_to :user
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_10_022043) do
+ActiveRecord::Schema.define(version: 2023_03_10_053601) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
