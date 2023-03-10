7ceecb07373a7ef26a22e85fe545dd86503ec1aa
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-10 14:30:07 +0900

    バリデーション、ストロングパラメーター作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,10 +4,18 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    
   end
 
+private
+
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_day_id, :price)
+  end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,13 @@
 class Item < ApplicationRecord
+
+  validates :name, presence: true
+  validates :explain, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
+
+
 end
