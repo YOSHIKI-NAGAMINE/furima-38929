9883b90bfc1fbd52eaa9fedd38b38dcc65f5de6a
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-11 15:58:20 +0900

    バリデーション並び替え

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,21 +1,20 @@
 class Item < ApplicationRecord
   
+  validates :image, presence: true, blob: { content_type: :image }
   validates :name, presence: true
   validates :explain, presence: true
+  validates :price, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
+  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" } 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 
-  validates :price, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
-  validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters" } 
-
 
   belongs_to :user
   has_one_attached :image
-  validates :image, presence: true, blob: { content_type: :image }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
