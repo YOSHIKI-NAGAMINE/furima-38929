f45396f1e3662fe57c9b1cfd9502e492aa7b8e23
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-12 19:36:09 +0900

    ルーティング、コントローラー作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -34,6 +34,10 @@ class ItemsController < ApplicationController
     else
       render action: :edit
     end
+  end
+
+  def destroy
+    @item.destroy
   end
 
   private
