3eed6042d34c990e63d43df24594121982c6b485
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-13 19:44:30 +0900

    バリデーション設定

--- app/models/order_order_info.rb
+++ app/models/order_order_info.rb
@@ -3,16 +3,18 @@ class OrderOrderInfo
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :manicipalities, :address, :building, :phone_number, :order_id, :item_id, :user_id
 
+  
+  validates :post_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :post_code
-    validates :prefecture_id
     validates :manicipalities
-    validates :address, presence: true
-    validates :phone_number
+    validates :address
   end
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  validates :phone_number, presence: true, format: {with: /\A[0-9]+\z/, message: 'is invalid. Input only number' }
+  validates :phone_number, format: {with: /\A\d{10,11}\z/, message: 'is too short' }
+
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
