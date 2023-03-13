e89a08000904dc644a3b264e68ef6f16d9f5134e
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-13 15:35:03 +0900

    購入者情報モデル、テーブル作成

---
+++ db/migrate/20230313060411_create_order_infos.rb
@@ -0,0 +1,16 @@
+class CreateOrderInfos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :order_infos do |t|
+
+
+      t.string     :postal_code,      null: false
+      t.integer    :prefecture,       null: false
+      t.string     :manicipalities,   null: false
+      t.string     :address,          null: false
+      t.string     :building
+      t.string     :phone_number,     null: false
+      t.references :order,            null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_12_113309) do
+ActiveRecord::Schema.define(version: 2023_03_13_060411) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -60,6 +60,19 @@ ActiveRecord::Schema.define(version: 2023_03_12_113309) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "order_infos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "postal_code", null: false
+    t.integer "prefecture", null: false
+    t.string "manicipalities", null: false
+    t.string "address", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_infos_on_order_id"
+  end
+
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "item_id", null: false
@@ -107,6 +120,7 @@ ActiveRecord::Schema.define(version: 2023_03_12_113309) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
+  add_foreign_key "order_infos", "orders"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
 end
