f035e2bf9e2ac775d94a4c9d7c2888d51f7f9aca
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-13 11:33:50 +0900

    購入情報orderの保存

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,20 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    binding.pry
+    @order.save
+    redirect_to root_path
+  end
+
+  private
+
+  def order_params
+    params.permit(:item_id).merge(user_id: current_user.id)
   end
 
 end
