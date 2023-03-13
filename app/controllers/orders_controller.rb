dd9e85becc9039312a0ed0a27c7ea2d4596c400c
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-03-13 16:53:09 +0900

    購入者情報の保存機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,8 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(order_params)
     @order.save
+    @order_info = OrderInfo.new(order_info_params)
+    @order_info.save
     redirect_to root_path
   end
 
@@ -21,6 +23,10 @@ class OrdersController < ApplicationController
 
   def order_params
     params.permit(:item_id).merge(user_id: current_user.id)
+  end
+
+  def order_info_params
+    params.permit(:post_code, :prefecture_id, :manicipalities, :address, :building, :phone_number).merge(order_id: @order.id)
   end
 
 end
